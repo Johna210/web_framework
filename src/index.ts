ddd3122e0507ced3c6d7cbf8cc91322c94bfaf1b
Johna210 <solomonjohna21@gmail.com>
2024-10-18 16:00:13 +0300

    Implemented re render in age change

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { User } from "./models/User";
 import { UserForm } from "./views/UserForm";
+
+const user = User.buildUser({ name: "NAME", age: 20 });
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
-  const userForm = new UserForm(rootElement);
+  const userForm = new UserForm(rootElement, user);
   userForm.render();
 } else {
   console.error("Root element not found");
